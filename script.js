81f66c1795a68c9ccd4b82b1c08da2a26fb294b5
Pritamdhar_official <pritamdhar6568@gmail.com>
2022-12-14 14:29:28 +0530

    option(onclick,correct,incorrect,tick and cross)
    Based on user click to options  build the logic to get correct ans as well as incorrect ans.Also provide tickicon and crossicon at last.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const info_box = document.querySelector(".info_box ");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const option_list= document.querySelector(".option_list");
 
 //if start button clicked
 start_btn.onclick=()=>{
@@ -39,7 +40,7 @@ next_btn.onclick=()=>{
 //getting question and opton from array
 function showQuestions(index){
     const que_text= document.querySelector(".que_text");
-    const option_list= document.querySelector(".option_list");
+    
     let que_tag='<span>'+ questions[index].numb+'.'+questions[index].question+'</span>';
     let option_tag='<div class="option">'+questions[index].options[0]+'<span></span></div>'
                  +'<div class="option">'+questions[index].options[1]+'<span></span></div>'
@@ -47,7 +48,51 @@ function showQuestions(index){
                  +'<div class="option">'+questions[index].options[3]+'<span></span></div>';
     que_text.innerHTML=que_tag;
     option_list.innerHTML=option_tag;
+
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+        
+    }
 }
+
+
+let tickIcon='<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon='<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+
+function optionSelected(answer){
+    let userAns= answer.textContent;
+    let CorrectAns =questions[que_count].answer;
+    let allOptions =option_list.children.length;
+    if(userAns == CorrectAns){
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    }else{
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+
+        //if answer is incorrect then show the correct answer automatically
+        for (let i = 0; i < allOptions; i++) {
+           if(option_list.children[i].textContent==CorrectAns) {
+            option_list.children[i].setAttribute("class","option correct");
+            option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+           }
+            
+        }
+    }
+}
+
+//disabled all option
+for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+    
+}
+
+
+
+
+
 
 function queCounter(index) {
     const bottom_que_counter= quiz_box.querySelector(".total_que");
